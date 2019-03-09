2b99b9b17aac68fe511b9cb411af7a144af2b3ee
joshua <nwankwo.joshua@gmail.com>
2019-03-09 11:26:03 +1100

    Updated stream test

--- src/main/java/com/avantsystems/problems/datastructures/StreamTest.java
+++ src/main/java/com/avantsystems/problems/datastructures/StreamTest.java
@@ -1,24 +1,37 @@
 package com.avantsystems.problems.datastructures;
 
+import java.awt.image.Kernel;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamTest {
 
 
-    static void execute(){
-        List<String> list = Arrays.asList("josh,zoe,asha,queda","2","zoe","jay","shane");
+    static void execute() {
+        List<String> list = Arrays.asList("josh,zoe,asha,queda", "2", "zoe", "jay", "shane");
         Stream<String> stream = list.stream();
         Optional<String[]> result = stream.findFirst().map(x -> x.split(","));
         String[] x = result.orElse(null);
         System.out.println(x.toString());
     }
 
+    static void stream2() {
+        Stream
+                .of("Josh", "Zoe", "Asha", "Maqueda", "Bodhi", "Natasha","Samuel")
+                .map(s -> {
+                    System.out.println("Map: " + s);
+                    return s.toUpperCase();
+                }).anyMatch(s -> {
+            System.out.println(s);
+            return s.startsWith("Z");
+        });
+    }
+
     public static void main(String[] args) {
-        StreamTest.execute();
+        //StreamTest.execute();
+        StreamTest.stream2();
     }
 
 }
