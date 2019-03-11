769297fd208aaa769080f19a16337a3c0b8d6bf6
Josh Uzo <nwankwo.joshua@gmail.com>
2019-03-11 18:25:48 +0800

    Added singly linked lists

---
+++ src/main/java/com/avantsystems/problems/datastructures/HackerRankLinkedList.java
@@ -0,0 +1,49 @@
+package com.avantsystems.problems.datastructures;
+
+public class HackerRankLinkedList {
+
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+    }
+
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        HackerRankLinkedList.SinglyLinkedListNode newNode = new HackerRankLinkedList.SinglyLinkedListNode(data);git stat
+        HackerRankLinkedList.SinglyLinkedListNode tail = head;
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (tail.next != null){
+                tail = tail.next;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        HackerRankLinkedList l = new HackerRankLinkedList();
+        HackerRankLinkedList.insertNodeAtTail(null, 89);
+
+    }
+
+
+}
--- src/main/java/com/avantsystems/problems/datastructures/SinglyLinkedList.java
+++ src/main/java/com/avantsystems/problems/datastructures/SinglyLinkedList.java
@@ -4,23 +4,114 @@ public class SinglyLinkedList<T> {
 
     Node<T> head;
     Node<T> tail;
+    int size = 0;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
         head = null;
         tail = null;
     }
 
+    public int getSize() {
+        return size;
+    }
+
     void add(T t) {
         Node<T> node = new Node<>();
-        if (head == null){
+        node.setValue(t);
+        if (head == null) {
             head = node;
             tail = node;
         } else {
             tail.setNext(node);
             tail = node;
         }
+        size++;
     }
 
+    void addFirst(T t) {
+        Node<T> node = new Node<>();
+        node.setValue(t);
+        if (head != null) {
+            node.setNext(head);
+            head = node;
+            size++;
+        }
+    }
+
+
+    void addLast(T t) {
+        Node<T> node = new Node<>();
+        node.setValue(t);
+        if (tail != null) {
+            tail.setNext(node);
+            tail = node;
+            size++;
+        }
+    }
+
+    //Retrieves and removes the head (first element) of this list.
+    void remove(){
+        if (head != null) {
+            head = head.getNext();
+            size--;
+        }
+    }
+
+    void removeLast(){
+        if(tail != null) {
+
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "SinglyLinkedList{" +
+                "head=" + head +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        list.add("josh");
+        list.add("josh2");
+        list.add("josh3");
+        list.add("josh4");
+        list.addFirst("josh0");
+        //System.out.println(list.toString());
+        list.addLast("josh9");
+        //System.out.println(list.toString());
+        //list.remove();
+        //System.out.println(list.toString());
+
+        Node temp = list.head;
+        System.out.println(temp.hashCode());
+        System.out.println(list.head.hashCode());
+        System.out.println("Tail hashcode " + list.tail.hashCode());
+
+
+        while (temp.getNext() != null){
+            //System.out.println(temp.getNext());
+            temp = temp.getNext();
+            System.out.println(temp.hashCode());
+        }
+
+        Node n = new Node();
+        n.setValue(200);
+
+        temp.setNext(n);
+        temp = n;
+
+        System.out.println(list.toString());
+        System.out.println(temp.toString());
+
+        //System.out.println(list.head.getNext().getNext());
+        //System.out.println(list.tail.getNext());
+        //System.out.println(list.tail.getValue());
+
+        //System.out.println(list.getSize());
+    }
 
 
 }
