1b370d2f57bcb6d4c492fdcd7ee4ab14fa2f3393
Josh Uzo <nwankwo.joshua@gmail.com>
2019-03-30 13:45:57 +1100

    Updated array reversal methods

--- src/main/java/com/avantsystems/problems/datastructures/arrays/ArraySort.java
+++ src/main/java/com/avantsystems/problems/datastructures/arrays/ArraySort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ArraySort {
 
-    static void toSortedState(int[] arr, int index){
+    static void swapUntilSortedState(int[] arr, int index){
         if (index == 0) return;
 
         if (arr[index] < arr[index-1]) {
@@ -17,26 +17,12 @@ public class ArraySort {
             //swap previous for current
             arr[index-1] = currVal;
 
-            System.out.println("Swapped " + Arrays.toString(arr));
+            System.out.println(Arrays.toString(arr));
 
-            toSortedState(arr, index-1);
-            //swapRecurse(arr, );
-        }
-
-
-
-    }
-
-
-    static void iterateBackwards(int[] arr, int index){
-        int tmp = arr[index];
-        for (int i = index-1; i > 0 ; i--) {
-            System.out.println("[" + i + "] " + arr[i]);
-            if(i >=0 && arr[i] < arr[i-1]) {
-                toSortedState(arr,i);
-            }
+            swapUntilSortedState(arr, index-1);
         }
     }
+
 
 
     static void sort(int[] arr){
@@ -46,7 +32,7 @@ public class ArraySort {
             int prevVal = arr[i-1];
             System.out.println("Iteration [" + i + "] ");
             //if the current item is greater than the previous then call swap method.
-                if((currVal < prevVal)) toSortedState(arr,i);
+                if((currVal < prevVal)) swapUntilSortedState(arr,i);
         }
 
     }
@@ -54,9 +40,9 @@ public class ArraySort {
     public static void main(String[] args) {
         //int[] arr = new int[]{1,2,3,4,5,6,0,9};
         int[] arr = new int[]{7, 1, 3, 2, 4, 5, 6};
-        System.out.println(Arrays.toString(arr));
+        //System.out.println(Arrays.toString(arr));
         ArraySort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        //System.out.println(Arrays.toString(arr));
         //ArraySort.iterateBackwards(arr,5);
     }
 }
